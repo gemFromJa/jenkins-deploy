8e8612184fb4b0c8afbcf304695b375a444ba5e3
Israel <stephankw16@gmail.com>
2021-09-12 14:54:38 -0500

    fix 'failing' test

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-    res.send("respond with a resource");
-    // res.status(200).send({
-    //     success: true,
-    //     data: [{ user: "tony" }, { user: "lisa" }],
-    // });
+    // res.send("respond with a resource");
+    res.status(200).send({
+        success: true,
+        data: [{ user: "tony" }, { user: "lisa" }],
+    });
 });
 
 module.exports = router;
